476e7bd532c0d20c55548a97cab5ec020d4eeb25
Subham Dey <sdey200406@gmail.com>
2025-09-30 11:55:55 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./pages/Dashboard/PowerBIDashboard";
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
