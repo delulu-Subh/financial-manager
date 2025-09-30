9b29ad968ba4ec7b5bebbf8c73d5a3d8cb0390b9
Subham Dey <sdey200406@gmail.com>
2025-09-30 11:57:46 +0530

    Create Header.js

---
+++ client/src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="p-4 bg-gray-900 text-white">
+      <h1 className="text-xl font-bold">Financial Manager</h1>
+    </header>
+  );
+};
+
+export default Header;
